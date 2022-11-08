0b006bce5b27e577b343286aa8dae252ebc2d814
Javier Lopez <jvrlpzd@gmail.com>
2022-11-08 14:09:34 +0100

    Added navbar links and necessary ID's

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './nav.css';
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BiBookBookmark} from 'react-icons/bi'
+import {RiServiceLine} from 'react-icons/ri'
+import {BiMessageDetail} from 'react-icons/bi'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#"><AiOutlineHome /></a>
+      <a href="#about"><AiOutlineUser/></a>
+      <a href="#experience"><BiBookBookmark/></a>
+      <a href="#services"><RiServiceLine/></a>
+      <a href="#contact"><BiMessageDetail/></a>
+    </nav>
   )
 }
 
