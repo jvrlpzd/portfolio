1bd2a474b36572e788f16e035ef03c8452493d28
Javier Lopez <jvrlpzd@gmail.com>
2022-11-13 23:30:14 +0100

    Finished, just needs changing texts, pictures and uploading

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 import './footer.css'
+import { BsLinkedin } from 'react-icons/bs'
+import { FaGithub } from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>JAVIER</a>
+
+      <ul className="permalinks">
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experiences">Experiences</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">About</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://github.com"><BsLinkedin /></a>
+        <a href="https://linkedin.com"><FaGithub /></a>
+      </div>
+
+<div className="footer__copyright">
+  <small>&copy; Javier Lopez Dominguez</small>
+</div>
+
+    </footer>
   )
 }
 
