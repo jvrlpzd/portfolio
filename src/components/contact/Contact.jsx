42ee293237b73074cd154b6a77800803a74e0d55
Javier Lopez <jvrlpzd@gmail.com>
2022-11-29 15:27:09 +0100

    Traduction composant contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import './contact.css';
-import {MdOutlineEmail} from 'react-icons/md'
-import {BsLinkedin} from 'react-icons/bs'
-import { useRef } from 'react';
-import emailjs from '@emailjs/browser';
+import React from "react";
+import "./contact.css";
+import { MdOutlineEmail } from "react-icons/md";
+import { BsLinkedin } from "react-icons/bs";
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
   const form = useRef();
@@ -11,54 +11,66 @@ const Contact = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_8vs17el', 'template_6j3ppzo', form.current, 'ADC_ej9Jy70EjfPwu')
-      .then((result) => {
+    emailjs
+      .sendForm(
+        "service_8vs17el",
+        "template_6j3ppzo",
+        form.current,
+        "ADC_ej9Jy70EjfPwu"
+      )
+      .then(
+        (result) => {
           console.log(result.text);
-      }, (error) => {
+        },
+        (error) => {
           console.log(error.text);
-      });
+        }
+      );
 
-      e.target.reset();
+    e.target.reset();
   };
 
   return (
     <section id="contact">
-      <h5>Get in Touch</h5>
-      <h2>Contact Me</h2>
+      <h5>Me joindre</h5>
+      <h2>Contact</h2>
 
       <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
-            <MdOutlineEmail className='contact__option-icon'/>
+            <MdOutlineEmail className="contact__option-icon" />
             <h4>Email</h4>
-            <h5>blablabla@gmail.com</h5>
-            <a href="mailto:blablabla@gmail.com" target='_blank'>Send a message</a>
+            <h5>javlopez.pro@gmail.com</h5>
+            <a href="mailto:javlopez.pro@gmail.com" target="_blank">
+              Envoyer un mail
+            </a>
           </article>
           <article className="contact__option">
-            <BsLinkedin className='contact__option-icon'/>
+            <BsLinkedin className="contact__option-icon" />
             <h4>LinkedIn</h4>
             <h5>Javier Lopez Dominguez</h5>
-            <a href="linkedin.com" target='_blank'>Send a message</a>
+            <a href="linkedin.com" target="_blank">
+              Contact via LinkedIn
+            </a>
           </article>
-          <article className="contact__option">
-            <MdOutlineEmail className='contact__option-icon'/>
-            <h4>Email</h4>
-            <h5>blablabla@gmail.com</h5>
-            <a href="mailto:blablabla@gmail.com" target='_blank'>Send a message</a>
-          </article>
-
         </div>
-{/* END OF CONTACT OPTIONS */}
-<form ref={form} onSubmit={sendEmail}>
-  <input type="text" name='name' placeholder='Your Full Name' required />
-  <input type='email' name='email' placeholder='Your Email' required />
-  <textarea name="message" rows="7" placeholder='Your Message' required ></textarea>
-  <button type='submit' className='btn btn-primary'>Send Message</button>
-</form>
-
+        {/* END OF CONTACT OPTIONS */}
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name="name" placeholder="Nom Prénom" required />
+          <input type="email" name="email" placeholder="Mail" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Envoyer
+          </button>
+        </form>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
